9d3eb072ab7bd2323c05934f1fe52aeeb759ff98
JWitcomb <31311127+JWitcomb@users.noreply.github.com>
2017-08-28 22:13:22 +0100

    Updated
    Lots of changes

--- Unity Challenge 2/Spy Hunter/Assets/Scripts/CharacterController.cs
+++ Unity Challenge 2/Spy Hunter/Assets/Scripts/CharacterController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
 
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
 
     [System.Serializable]
     public class Boundary
     {
         public float xMin, xMax, zMin, zMax;
     }
-
-
+    
     Rigidbody rBody;
 
 	void Start ()
@@ -19,19 +22,13 @@ public class CharacterController : MonoBehaviour {
        if (GetComponent<Rigidbody>())
             rBody = GetComponent<Rigidbody>();
     }
-
     
-
-    public float speed;
-    public float tilt;
-    public Boundary boundary;
-
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
         rBody.velocity = movement * speed;
 
         rBody.position = new Vector3
@@ -42,6 +39,19 @@ public class CharacterController : MonoBehaviour {
         );
 
         rBody.rotation = Quaternion.Euler(0.0f, 0.0f, rBody.velocity.x * tilt);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Obsticle")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+
     }
 
 
---
+++ Unity Challenge 2/Spy Hunter/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text score;
+
+    public static int myScore;
+
+    void Awake()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        score.text = "Score: " + myScore;
+    }
+}
