ed360c297bc9faaffcb93c718b300ec417732e29
JWitcomb <31311127+JWitcomb@users.noreply.github.com>
2017-10-09 17:31:15 +0100

    Additional Work
    LOL

---
+++ Unity Challenge 2/Spy Hunter/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    		
+	}
+
+    public void Pause()
+    {
+        if (isPaused == false)
+        {
+            isPaused = true;
+            pauseMenu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        else
+        {
+            isPaused = false;
+            pauseMenu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Unity Challenge 2/Spy Hunter/Assets/Scripts/ResumeTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeTime : MonoBehaviour {
+
+	
+	void Start ()
+    {
+        Time.timeScale = 1;
+    }
+	
+
+}
